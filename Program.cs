852f0cf999f7315f99f5dc234d8317c73f72abb3
Renato Bairos <67176383+renatobairos@users.noreply.github.com>
2021-06-21 23:30:41 -0300

    Finalizacao Soldado Rian

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FCamara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int qtdeSoldados;
+            int posicaoSobrevivente = 1;
+            int expoenteBase2;
+            int inicioSequenciaExpoente;
+
+            do {
+            Console.Write("Digite a quantidade de soldados => ");
+            qtdeSoldados = Convert.ToInt32(Console.ReadLine());
+
+            } while (qtdeSoldados < 1);
+
+            expoenteBase2 = (int) Math.Log2(qtdeSoldados);
+            inicioSequenciaExpoente = (int) Math.Pow(2, expoenteBase2);
+
+            for (var i = inicioSequenciaExpoente; i < qtdeSoldados; i++) {
+                posicaoSobrevivente += 2;
+            }
+
+            Console.WriteLine("Posição do soldado sobrevivente => " + posicaoSobrevivente);
+
+        }
+    }
+}
